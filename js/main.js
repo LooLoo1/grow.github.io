fec82e8c9972556e4d8689609c66c2ad006f3e3a
LooLoo1 <59607308+LooLoo1@users.noreply.github.com>
2021-08-22 23:07:19 +0300

    My first work
    • Worked with: ~~EN~~
    - HTML;
    - CSS, Sass (Present adaptive);
    - Js: slider, dashboard and navigation;
    - Layout: AdobeXD, Figma (Layout repeated in Figma and improved by adding a menu, slider, dashboard and page 404).
    
    • Работал с: ~~ RU ~~
    - HTML;
    - CSS, Sass (Присутствует адаптив)
    - Js: слайдер, информационная панель и навигация;
    - Макет: AdobeXD, Figma (Макет собственноручно повторил в Figma и усовершенствовал добавив меню, слайдер, информационную панель и страницу 404).

---
+++ js/main.js
@@ -0,0 +1,123 @@
+let b = document.querySelector('citeLoad');
+window.addEventListener("load", function (event) {
+	b.style.animation = "shadow-inset-center 2s ease-out forwards";
+});
+
+
+//Позиціонування дерева
+const marginLeft = document.querySelector(".conteiner");
+const tree = document.querySelector(".title-tree");
+window.addEventListener('resize', locationTree);
+window.onload = locationTree();
+
+function locationTree() {
+	const leftSize = marginLeft.offsetLeft;
+	tree.style.left = "-" + leftSize + 'px';
+}
+
+
+// Меню бургер
+const navBtn = document.querySelectorAll(".menu__btn");
+const navMobile = document.querySelector(".menu__mobile");
+const navBlur = document.querySelectorAll(".menu__list");
+
+for (let i = 0; i < navBtn.length; i++) {
+	navBtn[i].onclick = function () {
+		for (let i = 0; i < navBtn.length; i++) { navBtn[i].classList.toggle('active'); }
+		navMobile.classList.toggle('active');
+		navBlur[1].classList.toggle('active');
+	}
+}
+
+
+// Інформаційна панель
+const infoPanel = document.querySelector(".info__panel");
+const infoPBtn = document.querySelector(".info__panel .btn");
+const btnInfoPanel = [document.querySelector(".menu__list-btn"), document.querySelector(".information .btn")];
+infoPBtn.onclick = function () {
+	infoPanel.classList.toggle('active');
+}
+for (let i = 0; i < navBtn.length; i++) {
+	btnInfoPanel[i].onclick = function () {
+		infoPanel.classList.toggle('active');
+	}
+}
+
+
+
+
+// Слайдер
+const arrowNext = document.querySelector(".arrow__next");
+const arrowPrev = document.querySelector(".arrow__prev");
+const sliderItem = document.querySelectorAll(".slider__bg-img");
+let count = 0;
+
+arrowNext.onclick = function () {
+	forBG();
+	countAdd();
+	addBG();
+}
+
+function countAdd() {
+	if (count + 1 < sliderItem.length) {
+		count++;
+	} else { count = 0; }
+}
+
+arrowPrev.onclick = function () {
+	forBG()
+	if (count - 1 < 0) {
+		count = sliderItem.length - 1;
+	} else { count--; }
+	addBG();
+}
+
+function forBG() {
+	for (let i = 0; i < sliderItem.length; i++) {
+		sliderItem[i].classList.remove('active');
+	}
+}
+
+function addBG() {
+	sliderItem[count].classList.add('active');
+	if (count == 0) {
+		tree.classList.add('active')
+	} else { tree.classList.remove('active') }
+}
+
+setInterval(() => {
+	forBG();
+	countAdd();
+	addBG();
+}, 15000);
+
+
+
+
+
+// Розміри монітора
+// let screenSize = document.createElement("WindowSize");
+// document.body.append(screenSize);
+// style(screenSize);
+
+// window.addEventListener('resize', screenResize);
+// window.onload = screenResize();
+
+// function screenResize() {
+// 	let als = [window.innerWidth, window.innerHeight]
+// 	screenSize.innerHTML = `${als[0]}<br>${als[1]}`;
+// }
+// function style(el) {
+// 	let css = el.style;
+// 	css.fontSize = "4vh";
+// 	css.padding = "0.5em 1em";
+// 	css.position = "absolute";
+// 	css.top = "0";
+// 	css.right = "0";
+// 	css.zIndex = "100000";
+// 	css.pointerEvents = "none";
+// 	css.borderBottomLeftRadius = "30%";
+// 	css.textAlign = "center";
+// 	css.background = "rgba(0, 0, 0, 0.5)";
+// }
+
