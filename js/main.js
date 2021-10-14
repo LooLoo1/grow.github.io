df9ad40b167ff4f720202a62f7458666697014fc
LooLoo1 <59607308+LooLoo1@users.noreply.github.com>
2021-10-14 14:03:20 +0300

    Optimization
    - Optimized pictures
    - Added support for background filter for older browsers
    - Minimized styles and JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let b = document.querySelector('citeLoad');
+let b = document.querySelector('#citeLoad');
 window.addEventListener("load", function (event) {
 	b.style.animation = "shadow-inset-center 2s ease-out forwards";
 });
@@ -90,34 +90,3 @@ setInterval(() => {
 	countAdd();
 	addBG();
 }, 15000);
-
-
-
-
-
-// Розміри монітора
-// let screenSize = document.createElement("WindowSize");
-// document.body.append(screenSize);
-// style(screenSize);
-
-// window.addEventListener('resize', screenResize);
-// window.onload = screenResize();
-
-// function screenResize() {
-// 	let als = [window.innerWidth, window.innerHeight]
-// 	screenSize.innerHTML = `${als[0]}<br>${als[1]}`;
-// }
-// function style(el) {
-// 	let css = el.style;
-// 	css.fontSize = "4vh";
-// 	css.padding = "0.5em 1em";
-// 	css.position = "absolute";
-// 	css.top = "0";
-// 	css.right = "0";
-// 	css.zIndex = "100000";
-// 	css.pointerEvents = "none";
-// 	css.borderBottomLeftRadius = "30%";
-// 	css.textAlign = "center";
-// 	css.background = "rgba(0, 0, 0, 0.5)";
-// }
-
